4a13738b7bad7cfdcecca1bc9bc8dbe1033c66eb
Scott Goudreau <egoud097@uottawa.ca>
2024-10-30 22:11:14 -0400

    added 2D VMI analysis code

---
+++ VMI_Functions.py
@@ -0,0 +1,475 @@
+import os
+import glob
+import numpy as np
+import scipy as sp
+import re
+import imutils
+import imageio.v2 as imio
+import matplotlib.pyplot as plt
+from timeit import default_timer as timer
+from scipy.signal import fftconvolve
+
+import DAVIS
+
+# Maps file types to a numerical value
+# which is the decimal equivalent of the binary code peb
+# where p=pump, e=probe, b=molecular beam
+pebmap = {'.img':7, 'img':7,
+          '.bgb':6, 'bgb':6, '.bgr':6, 'bgr':6,
+          'imgp':5, 'bgbp':4, 'imge':3, 'bgbe':2}
+
+# Extracts the delay in ps from the file name
+def str2delay(name):
+    t1 = [m.start() for m in re.finditer('\.', name)]
+    t2 = [m.start() for m in re.finditer('ps', name)]
+    s = name[t1[0]+1:t2[-1]]
+    return float(s)
+
+# Converts input scans string into an array of scan indices
+def scans2range(scans, scansN):
+    if scans[0] == -1: # returns all scans
+        return np.arange(1, scansN+1)
+    
+    s = np.array(())
+    for i in scans:
+        if np.size(i)==1: # include single scan
+            s = np.append(s, i)
+        if np.size(i)==2: # include range of scans
+            s = np.append(s, np.arange(i[0],i[1]))
+        else:
+            continue
+    return s
+
+# Constructs a dictionary containing all data extracted from the scans
+def vmigetstats(path, scans, storeimgs=False):
+    
+    # Make list of files in path
+    files = glob.glob(path+'/*.png')
+    if len(files)==0:
+        print("No .png files were found in {}".format(path))
+        raise SystemExit    
+    files.sort()
+    
+    print("Building database of files...")
+    s = timer()
+    
+    # Get list of delays from file names
+    delays = []
+    for i in range(0, len(files)):
+        fname = files[i]
+        delay_ps = str2delay(fname[fname.rindex('/')+1:])
+        if delay_ps == None:
+            continue
+        if not delay_ps in delays:
+            delays.append(delay_ps)
+    delaysN = len(delays)
+    
+    # Relic from Andrey's code - why reverse sort?
+    if delaysN > 1:
+        if delays[0] > delays[1]:
+            delays.sort(reverse=True)
+        else:
+            delays.sort()
+    #delays = np.array(delays, dtype=np.float32) # Removed to avoid floating point errors - 20200206
+    
+    db = []
+    scansN = 1
+    j = 0
+    delay_ps = None
+    
+    # Build database of files
+    for i in range(0, len(files)):
+        fname = files[i]
+        peb = pebmap[fname[-8:-4]]
+        if peb==6:
+            tmp = delay_ps
+            
+        delay_ps = str2delay(fname)
+        if delay_ps == None:
+            continue
+        
+        if peb==6:
+            if (delay_ps==tmp) or ( ( (delay_ps==delays[-1]) or (delay_ps==delays[0]) ) and ( (tmp==delays[-1]) or (tmp==delays[0]) ) ):
+                scansN = scansN + 1
+        
+        j = j + 1
+        db.append({'idx':j, 'del':delay_ps, 'peb':peb, 'scan':scansN, 'fname':fname})
+    print("Done. Time elapsed: {}".format(timer() - s));
+    
+    scans = scans2range(scans, scansN)
+    #print(scans)
+    
+    print("Extracting traces...")
+    s = timer()
+    
+    # Create mask to pull out requested scans
+    mask = np.zeros(len(db))
+    dbscan = [db[i]['scan'] for i in range(0,len(db))]
+    for i in range(0, len(scans)):
+        mask = np.logical_or(mask,(dbscan==scans[i]))
+    
+    # defining structures
+    traces = [np.empty((0,3)) for i in range(0,7)]
+    
+    weights = {7:np.zeros(delaysN),
+               6:np.zeros(delaysN),
+               5:np.zeros(delaysN),
+               4:np.zeros(delaysN),
+               3:np.zeros(delaysN),
+               2:np.zeros(delaysN)}
+    
+    avg = {7:0,
+           6:0,
+           5:0,
+           4:0,
+           3:0,
+           2:0}
+    
+    stk = {7:[0.0 for i in range(0, delaysN)],
+           6:[0.0 for i in range(0, delaysN)]}
+    
+    if storeimgs:
+        dim = np.shape(imio.imread(db[0]['fname']))
+        imgs = np.zeros((delaysN, np.size(scans), dim[0], dim[1]))
+    
+    niter = 0
+    nimg = np.size(mask[mask==True])
+    
+    # Read in and store each requested image:
+    for i in np.where(mask==True)[0]:
+        if (niter % 50 == 0):
+            print("#{} - ({}/{})".format(i, niter, nimg))
+        
+        case = db[i]['peb']
+        data = imio.imread(db[i]['fname']).astype('float64')
+        #idelay = np.where(delays==db[i]['del'])[0][0]; # Removed since delays is now a list - 20200206
+        idelay = delays.index(db[i]['del']) # index of current delay
+        j = delaysN * (db[i]['scan'] - 1) + idelay; # scan and delay index
+        
+        # (scan/delay index, image sum, image number)
+        nrow = (np.double(j) + 1, np.sum(data), np.double(i) + 1)
+        traces[7 - case] = np.vstack((traces[7 - case], nrow));
+        
+        # count number of images of each type/delay for averaging
+        weights[case][idelay] = weights[case][idelay] + 1
+        avg[case] = avg[case] + np.double(data)
+        
+        if case==7 or case==6: # img or bgb
+            stk[case][idelay] = stk[case][idelay] + np.double(data)
+            if storeimgs:
+                iscan = np.where(scans==dbscan[i])[0][0]
+                imgs[idelay, iscan] = imgs[idelay, iscan] + (2 * case - 13)*np.double(data)
+        niter = niter + 1
+
+    # perform averaging
+    for i in weights:
+        if not np.sum(weights[i]) == 0:
+            avg[i] = avg[i] / np.sum(weights[i])
+    for i in range(0, delaysN): 
+        if not weights[7][i] == 0:
+            stk[7][i] = stk[7][i] / weights[7][i]
+        if not weights[6][i] == 0:
+            stk[6][i] = stk[6][i] / weights[6][i]
+    
+    print("Done. Time elapsed: {}".format(timer() - s))
+            
+    print("Finding image centre...")
+    s = timer()
+    
+    data = avg[7]
+    if not len(avg[6])==1:
+        data = data - avg[6]
+        
+    conv = fftconvolve(data, data)
+    (x0, y0) = np.where(conv==np.max(conv))
+    x0 = int(x0[0]/2)
+    y0 = int(y0[0]/2)
+    print("Done. Time elapsed: {}".format(timer() - s))
+    print("Centre: {}, {}".format(x0,y0))
+
+
+    # notes below are differences from Matlab version
+    stats = {'folder':path,
+             'subfolder':path[path.rindex('/')+1:],
+             'delays':delays,
+             'scans':scans,
+             'imsize':np.shape(avg[7]),
+             'vmicentre':[x0,y0],
+             'traces':traces,
+             'imavgs':avg,
+             'imstks':stk,
+             'weights':weights, # included all not just img and bgb
+             'db':db, # fname is the full path
+             'storeimgs':storeimgs}
+    
+    if storeimgs:
+        stats['imgs'] = imgs.astype('int') # img and bgb reversed
+             
+    return stats
+
+
+# Plots yield to pick out good scans
+def vmiplottraces(stats):
+    
+    traces = stats['traces']
+    pname = stats['subfolder']
+    
+    fig, ax = plt.subplots()
+    ax.set(xlabel = "image #", ylabel = "total e yield", title = pname)
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    markers = ['.','.','s','o','x','+']
+    labels = ['111', '110', '101', '100', '011', '010']
+    plotparams = {'lw':1, 'ms':5, 'mfc':'none'}
+    
+    for i in range(0, 7):
+        if np.size(traces[i]) > 0:
+            ax.plot(traces[i][:,2], traces[i][:,1], label=labels[i],
+                    marker=markers[i], **plotparams)
+            
+    leg = ax.legend()
+    leg.set_draggable(1)
+    
+    
+# Plots various background subtractions
+def vmiplotaverages(stats):
+    
+    img = stats['imavgs'][pebmap['img']]
+    bgb = stats['imavgs'][pebmap['bgb']]
+    imgp = stats['imavgs'][pebmap['imgp']]
+    bgbp = stats['imavgs'][pebmap['bgbp']]
+    imge = stats['imavgs'][pebmap['imge']]
+    bgbe = stats['imavgs'][pebmap['bgbe']]
+    subfolder = stats['subfolder']
+    
+    isimgp = np.size(imgp) > 1
+    isimge = np.size(imge) > 1
+    
+    
+    # Figure 1
+    l = 1 + isimgp + isimge
+    fig1, ax1 = plt.subplots(l,3)
+    fig1.suptitle(subfolder + ": average images (1)")
+    if l==1:
+        ax1[0].imshow(img); ax1[0].set(title="img"); ax1[0].axis('off');
+        ax1[1].imshow(bgb); ax1[1].set(title="bgb"); ax1[1].axis('off');
+        ax1[2].imshow(img-bgb); ax1[2].set(title="img-bgb"); ax1[2].axis('off');
+    else:
+        ax1[0,0].imshow(img); ax1[0,0].set(title="img"); ax1[0,0].axis('off');
+        ax1[0,1].imshow(bgb); ax1[0,1].set(title="bgb"); ax1[0,1].axis('off');
+        ax1[0,2].imshow(img-bgb); ax1[0,2].set(title="img-bgb"); ax1[0,2].axis('off');
+        if isimgp:
+            ax1[1,0].imshow(imgp); ax1[1,0].set(title="imgp"); ax1[1,0].axis('off');
+            ax1[1,1].imshow(bgbp); ax1[1,1].set(title="bgbp"); ax1[1,1].axis('off');
+            ax1[1,2].imshow(imgp-bgbp); ax1[1,2].set(title="imgp-bgbp"); ax1[1,2].axis('off');
+        if isimge:
+            ax1[2,0].imshow(imge); ax1[2,0].set(title="imge"); ax1[2,0].axis('off');
+            ax1[2,1].imshow(bgbe); ax1[2,1].set(title="bgbe"); ax1[2,1].axis('off');
+            ax1[2,2].imshow(imge-bgbe); ax1[2,2].set(title="imge-bgbe"); ax1[2,2].axis('off');
+    
+    # Figure 2
+    fig2, ax2 = plt.subplots(2,2)
+    fig2.suptitle(subfolder + ": average images (2)")
+    ax2[0,0].imshow(img-bgb); ax2[0,0].set(title="img-bgb"); ax2[0,0].axis('off');
+    if isimgp:
+        ax2[0,1].imshow(img-bgb-imgp+bgbp);
+        ax2[0,1].set(title="(img-bgb)-(imgp-bgbp)"); ax2[0,1].axis('off');
+    if isimge:
+        ax2[1,0].imshow(img-bgb-imge+bgbe);
+        ax2[1,0].set(title="(img-bgb)-(imge-bgbe)"); ax2[1,0].axis('off');
+    if isimgp and isimge:
+        ax2[1,1].imshow(img-bgb-imgp+bgbp-imge+bgbe);
+        ax2[1,1].set(title="(img-bgb)-(imgp-bgbp)-(imge-bgbe)");
+        ax2[1,1].axis('off');
+    
+    # Figure 3
+    centre = np.flip(stats['vmicentre'])
+    fig3, ax3 = plt.subplots()
+    fig3.suptitle(subfolder + ": show image centre")
+    ax3.imshow(img-bgb-imgp+bgbp)
+    ax3.set(title="(img-bgb)-(imgp-bgbp)")
+    c1 = plt.Circle(centre, 220, facecolor='none', edgecolor='red'); ax3.add_patch(c1);
+    c2 = plt.Circle(centre, 150, facecolor='none', edgecolor='red'); ax3.add_patch(c2);
+    c3 = plt.Circle(centre, 100, facecolor='none', edgecolor='red'); ax3.add_patch(c3);
+    ax3.axvline(x=centre[0], color='red', ls=':')
+    ax3.axhline(y=centre[1], color='red', ls=':')
+            
+# Subtract the chosen background
+def vmibgsubtr(stats, peb):
+    print("Subtracting background...")
+    s = timer()
+    
+    delaysN = np.size(stats['delays'])
+    imsub = stats['imstks'][pebmap['img']]
+    
+    # no subtraction
+    if peb == '000':
+        stats['imstks']['difstk'] = imsub
+        imav = np.zeros(stats['imsize'])
+        for i in range(0, delaysN):
+            imav = imav + imsub[i] / delaysN
+        stats['imavgs']['difstk'] = imav
+        return stats
+    
+    for i in range(0, delaysN):
+        
+        if int(peb[0]):
+            imsub[i] = imsub[i] - (stats['imavgs'][pebmap['imgp']] - stats['imavgs'][pebmap['bgbp']])
+        if int(peb[1]):
+            imsub[i] = imsub[i] - (stats['imavgs'][pebmap['imge']] - stats['imavgs'][pebmap['bgbe']])
+        if int(peb[2]):
+            imsub[i] = imsub[i] - stats['imstks'][pebmap['bgb']][i]
+    
+    imav = np.zeros(stats['imsize'])
+    
+    for i in range(0, delaysN):
+        imav = imav + imsub[i] / delaysN
+        
+    stats['imstks']['difstk'] = imsub
+    stats['imavgs']['difstk'] = imav
+    
+    print("Done. Time elapsed: {}".format(timer() - s))
+    return stats
+
+# Crop images around centre
+def vmicropdifstk(stats, cropsize=-1):
+    print("Cropping images...")
+    s = timer()
+    
+    [x0, y0] = stats['vmicentre']
+    [sx, sy] = stats['imsize']
+    delaysN = np.size(stats['delays'])
+    
+    hc = 0
+    if np.size(cropsize)==1:
+        hc = np.min([x0-1, sx-x0, y0-1, sy-y0])
+    else:
+        hc = np.min(cropsize) / 2
+    
+    im = 'imstks'
+    df = 'difstk'
+    av = 'imavgs'
+    for i in range(0, delaysN):
+        stats[im][df][i] = stats[im][df][i][x0-hc:x0+hc, y0-hc+2:y0+hc+2]
+    
+    stats[av][df] = stats[av][df][x0-hc:x0+hc, y0-hc+2:y0+hc+2]
+    stats['imsize_crop'] = np.shape(stats[im][df][0])
+
+    print("Done. Time elapsed: {}".format(timer() - s))
+    return stats
+
+# Determines the rotation angle for alignment and applies the rotation
+def vmirotate(stats, angle, plotalign=False, lockangle=False):
+    
+    print("Determining image rotation angle..")
+    s = timer()
+    
+    if lockangle:
+        print("Images will be rotated by {} deg *WITHOUT OPTIMIZATION*".format(angle))
+        for i in range(0, np.size(stats['delays'])):
+            stats['imstks']['difstk'][i] = imutils.rotate(stats['imstks']['difstk'][i], angle)
+        stats['imavgs']['difstk'] = imutils.rotate(stats['imavgs']['difstk'], angle)
+        stats['angle'] = angle
+        return stats
+    
+    d = stats['imsize_crop'][0]
+    im = stats['imavgs']['difstk']
+    imin = im + np.flipud(np.fliplr(im)) # Symmetrize first
+    ROI = DAVIS.create_circular_mask(d, d, rin=0, rout=150) # Get mask between rin and rout
+    delaysN = np.size(stats['delays'])
+    
+    align = np.empty((0, 2))
+    # Step through angles, rotate, and compare top and bottom
+    for ang in np.arange(angle-30, angle+30, 0.5):
+        imr = imutils.rotate(imin, ang)
+        imr = imr*ROI
+        al = np.mean((imr[:int(d/2),:] - np.flipud(imr[int(d/2):,:])) ** 2)
+        align = np.vstack((align, [ang, al]))
+    
+    if plotalign: # Visualize curve for testing
+        plt.plot(align[:,0],align[:,1], marker='.')
+    
+    # Get best angle
+    rotanglei = np.where(align[:,1]==np.min(align[:,1]))
+    rotangle = align[rotanglei[0],0][0]
+    
+    print("Done. Time elapsed: {}".format(timer() - s))
+    print("Images will be rotated by {} deg".format(rotangle))
+    
+    # Rotate the images
+    for i in range(0, delaysN):
+        stats['imstks']['difstk'][i] = imutils.rotate(stats['imstks']['difstk'][i], rotangle)
+        
+    stats['imavgs']['difstk'] = imutils.rotate(stats['imavgs']['difstk'], rotangle)
+    stats['angle'] = rotangle
+    
+    return stats
+
+# Apply the inverse Able transform using the DAVIS algorithm
+def vmiiabel_stk(stats, msize, angstep, radstep, smoothing):
+    print("Inverting images...")
+    s = timer()
+    
+    delays = stats['delays']
+    delaysN = np.size(delays)
+    
+    inlen = stats['imsize_crop'][0]
+    rlen = int(inlen/(2*radstep))
+    
+    r = radstep*np.linspace(0, rlen-1, rlen)
+    
+    # Get the necessary matrices for DAVIS
+    (LMatrix, ms) = DAVIS.loadmats(msize, angstep, radstep, 180, inlen)
+    
+    Ir = np.zeros((delaysN, rlen))
+    iminstk = np.zeros((delaysN, inlen, inlen))
+    imout = np.zeros((delaysN, inlen, inlen))
+    iminpol = np.zeros((delaysN, 180, int(inlen/(2*radstep))))
+    imoutpol = np.zeros((delaysN, 180, int(inlen/(2*radstep))))
+    
+    # Smooth each image, symmetrize, and transform
+    for i in range(0, delaysN):
+        imin = stats['imstks']['difstk'][i]
+        imin = sp.ndimage.gaussian_filter(imin,(smoothing,smoothing))
+        imin = ((imin + np.fliplr(imin)) + 
+                np.flipud((imin + np.fliplr(imin)))) / 4
+        iminstk[i] = imin
+        (f, delta, iminpol[i], imoutpol[i], imout[i]) = DAVIS.transform(imin, LMatrix, ms, angstep, radstep)
+        Ir[i] = f[0]
+        print("{}\t{}ps\t{}\t{}".format(i, delays[i], np.sum(imin), np.sum(Ir[i,:])))
+    
+    stats['imstks']['iminstk'] = iminstk
+    stats['imstks']['idifstk'] = imout
+    stats['imstks']['iminpol'] = iminpol
+    stats['imstks']['imoutpol'] = imoutpol
+    stats['Ir'] = Ir
+    stats['r'] = r
+    
+    print("Done. Time elapsed: {}".format(timer() - s))
+    
+    return stats
+
+#%%
+    
+def vmical(file):
+
+    if not os.path.isfile(file):
+        print("Folder {} not found".format(file))
+        raise SystemExit;
+    
+    fid = open(file, 'r')
+    lines = fid.readlines()
+    fid.close()
+    
+    fmt = lines[0][1:-1]
+    
+    K = 0
+    if fmt=='r1,r2,dKE':
+        r1 = float(lines[1][:-1])
+        r2 = float(lines[2][:-1])
+        dE = float(lines[3][:lines[3].index(' ')])
+        K = dE / (r2**2 - r1**2)
+    else:
+        print("Format not supported (yet)")
+    
+    return K
+     
---
+++ VMI_Scripts.py
@@ -0,0 +1,284 @@
+# Imports
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d # This IS being used, even though Spyder doesn't think so
+from scipy.interpolate import interp1d
+from VMI_Functions import (vmigetstats, vmibgsubtr, vmicropdifstk,
+                          vmirotate, vmiiabel_stk, vmiplotaverages,
+                          vmiplottraces, vmical)
+
+#%%
+
+plt.close('all')
+
+#%%
+
+path = "20241010/20241010_cs2test3"
+inpath = r"C:\Users\Scott\Python\VMI\data/" + path
+outpath = r"C:\Users\Scott\Python\VMI\reduceddata/" + path
+
+# [-1] for 'all scans', or [scan, [start, stop], [start, stop]] ex [0, [3, 50], [60, 100]] Stop not inclusive
+scans = [-1]
+#scans = [15, [20,22]]
+
+if not os.path.isdir(inpath):
+    print("Folder {} not found".format(inpath))
+    raise SystemExit;
+
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+
+vmistats = vmigetstats(inpath, scans, 0);
+
+vmiplottraces(vmistats)
+vmiplotaverages(vmistats)
+
+#%%
+
+peb = '001'
+
+# peb = '000' - no bg subtraction
+#       '001' - subtract 'nobeam'
+#       '100' - subtract 'pumponly'
+#       '101' - subtract 'pumponly' and 'nobeam'
+#       '010' - subtract 'probeonly'
+#       '011' - subtract 'probeonly' and 'nobeam'
+#       '111' - subtract 'pumponly', 'probeonly' and 'nobeam'
+
+#vmistats['vmicentre'] = [234, 355]
+
+vmistats = vmibgsubtr(vmistats, peb)
+vmistats = vmicropdifstk(vmistats)
+
+#%%
+
+vmistats = vmirotate(vmistats, 62.5+90, lockangle=True)
+#vmistats = vmirotate(vmistats, 70)
+
+#%%
+
+vmistats = vmiiabel_stk(vmistats, 3000, 1, 0.2, 0)
+
+#%% Test Transformation
+
+Ir = vmistats['Ir']
+delays = vmistats['delays']
+r = vmistats['r']
+
+imraw = vmistats['imstks'][7]
+imin = vmistats['imstks']['iminstk']
+iminpol = vmistats['imstks']['iminpol']
+imoutpol = vmistats['imstks']['imoutpol']
+imout = vmistats['imstks']['idifstk']
+
+fig, axs = plt.subplots(2,2)
+
+i = 4
+
+axs[0,0].imshow(imin[i])
+axs[0,1].imshow(iminpol[i])
+axs[1,0].imshow(imoutpol[i])
+axs[1,1].imshow(imout[i])
+
+plt.figure()
+plt.imshow(imraw[i])
+
+plt.figure()
+plt.plot(r, Ir[i])
+
+#%% plot TR-PES (r[pix],t[ps])
+
+plot3d = True
+
+Ir = vmistats['Ir']
+delays = vmistats['delays']
+r = vmistats['r']
+
+upperlimit_percent_2d = 100
+
+fig = plt.figure()
+if len(delays)>1:
+    if plot3d:
+        ax = plt.axes(projection="3d")
+        X, Y = np.meshgrid(r, delays);
+        ax.plot_surface(X, Y, Ir, cmap='nipy_spectral')
+        ax.set_zlabel("Intensity")
+    else:
+        ax = plt.axes()
+        X, Y = np.meshgrid(r, delays);
+        ax.pcolormesh(X, Y, Ir, cmap='nipy_spectral', vmax=np.percentile(Ir,upperlimit_percent_2d))
+    ax.set_ylabel("delays, [ps]")
+else:
+    plt.plot(r, Ir)
+ax.set_xlabel("ring radius, [pix]")
+ax.set_title("TRPES")
+
+fout = outpath + "/" + vmistats['subfolder'] + ".trpes.r[pix].csv"
+out = np.vstack((np.hstack((2, r)), np.vstack((delays, Ir.T)).T))
+np.savetxt(fout, out, fmt='%.2f', delimiter=",")
+
+#%% TR-PES (r[pix],t[ps]): plot time evolutions of few energy-slices 
+
+rbinsa = [[1, 30], [31, 100], [150, 200]] #pixels
+r = vmistats['r']
+delays = vmistats['delays']
+Ir = vmistats['Ir']
+
+irbinsa = [np.searchsorted(r, n) for n in rbinsa]
+
+Ir_rbinsa = np.zeros((Ir.shape[0],len(rbinsa)))
+labels = []
+for i in range(0, len(rbinsa)):
+    Ir_rbinsa[:,i] = np.sum(Ir[:,irbinsa[i][0]:irbinsa[i][1]],1)/(irbinsa[i][1]-irbinsa[i][0]+1)  
+    labels.append("r[pix]= {}...{}".format(rbinsa[i][0], rbinsa[i][1]))
+
+fig, ax = plt.subplots()
+ax.set(xlabel = "delays, [ps]", title = "Evolution of trpes slices with p-p delay")
+    
+for i in range(0, len(rbinsa)):
+        ax.plot(delays, Ir_rbinsa[:,i], label=labels[i])
+            
+leg = ax.legend()
+leg.set_draggable(1)
+
+#%% TR-PES (r[pix],t[ps]): plot PES at few pp-delays 
+
+dbinsa = [[-0.1, 0.1], [-0.5, -0.2], [-2.0, -0.8]] #ps
+r = vmistats['r']
+delays = vmistats['delays']
+Ir = vmistats['Ir']
+
+idbinsa = [len(delays)-np.searchsorted(delays[::-1], n)[::-1] for n in dbinsa]
+
+Ir_dbinsa = np.zeros((len(dbinsa),Ir.shape[1]))
+labels = []
+for i in range(0, len(dbinsa)):
+    Ir_dbinsa[i,:] = np.sum(Ir[idbinsa[i][0]:idbinsa[i][1],:],0)/(idbinsa[i][1]-idbinsa[i][0]+1)  
+    labels.append("[{}...{}]ps".format(dbinsa[i][0], dbinsa[i][1]))
+
+fig, ax = plt.subplots()
+ax.set(xlabel = "r, [pix]", title = "Evolution of trpes slices with p-p delay")
+    
+for i in range(0, len(dbinsa)):
+        ax.plot(r, Ir_dbinsa[i,:], label=labels[i])
+            
+leg = ax.legend()
+leg.set_draggable(1)
+
+#%% plot TR-PES (ke[eV],t[ps])
+
+#calfile = '20190612_01_xenon_cal'
+calfile = '03_xenon.vmical_ml.txt'
+plot3d = True
+upperlimit_percent_2d = 100
+
+Ir = vmistats['Ir']
+r = vmistats['r']
+delays = vmistats['delays']
+delaysN = len(delays)
+#K = vmical(calfile)
+
+#nm160 = 160.9
+#xeip = 12.1299
+cs2ip = 10.073
+h = 4.135667516e-15 # eV*s
+c = 299792458 # m/s
+E200 = h*c/(200*1e-9)
+E267 = h*c/(267*1e-9)
+posE = 2*E200-cs2ip
+radius = 170
+
+K = posE / radius**2 #0.002264
+
+KEs = np.arange(0, 1.1, 0.005)
+bins = (KEs/K)**(0.5)
+
+Ir_ke = np.zeros((Ir.shape[0], len(bins)))
+    
+for i in range(0, Ir.shape[0]):
+    f = interp1d(r, Ir[i], fill_value='extrapolate')
+    Ir_ke[i] = f(bins)
+
+fig = plt.figure(1)
+#fig.canvas.set_window_title("TRPES: {}".format(vmistats['subfolder']))
+
+if delaysN>1:
+    if plot3d:
+        ax = plt.axes(projection="3d")
+        X, Y = np.meshgrid(KEs, delays);
+        ax.plot_surface(X, Y, Ir_ke, cmap='nipy_spectral')
+        ax.set_zlabel("Intensity")
+    else:
+        ax = plt.axes()
+        X, Y = np.meshgrid(KEs, delays);
+        ax.pcolormesh(X, Y, Ir_ke, cmap='nipy_spectral', vmax=np.percentile(Ir_ke,upperlimit_percent_2d))
+    ax.set_ylabel("delays, [ps]")
+else:
+    plt.plot(KEs, Ir_ke)
+ax.set_xlabel("eKE, [eV]")
+ax.set_title("TRPES")
+
+vmistats['Ir_ke'] = Ir_ke
+vmistats['ke'] = KEs
+vmistats['K'] = K
+
+#fout = outpath + "/" + vmistats['subfolder'] + ".trpes.ke.csv"
+#out = np.vstack((np.hstack((0, KEs)), np.vstack((delays, Ir_ke.T)).T))
+#np.savetxt(fout, out, fmt='%.2f', delimiter=",")
+
+#%% TR-PES (ke[eV],t[ps]): plot time evolutions of few energy-slices 
+
+kebinsa = [[0.02, 0.2], [0.3, 0.5], [0.5, 0.8], [0.02, 1.0]]
+
+KEs = vmistats['ke']
+delays = vmistats['delays']
+Ir_ke = vmistats['Ir_ke']
+K = vmistats['K']
+
+def ke2r(ke, K): return np.round((ke/K)**(0.5))
+def r2ke(r, K): return K*r**2
+
+irbinsa = [np.searchsorted(r, ke2r(np.array(n), K)) for n in kebinsa]
+
+Ir_rbinsa = np.zeros((Ir.shape[0],len(kebinsa)))
+labels = []
+for i in range(0, len(kebinsa)):
+    Ir_rbinsa[:,i] = np.sum(Ir[:,irbinsa[i][0]:irbinsa[i][1]],1)  
+    labels.append("ke[eV]= {}...{}".format(kebinsa[i][0], kebinsa[i][1]))
+
+fig, ax = plt.subplots()
+ax.set(xlabel = "delays, [ps]", title = "Evolution of trpes slices with p-p delay")
+    
+Ir_rbinsa = Ir_rbinsa / (np.max(Ir_rbinsa) - np.min(Ir_rbinsa))
+
+for i in range(0, len(kebinsa)):
+        ax.plot(delays, Ir_rbinsa[:,i], label=labels[i])
+            
+leg = ax.legend()
+leg.set_draggable(1)
+
+#%% TR-PES (ke[eV],t[ps]): plot PES at few pp-delays 
+
+dbinsa = [[-0.1, 0.1], [-0.5, -0.2], [-2.0, -0.8]] #ps
+KEs = vmistats['ke']
+delays = vmistats['delays']
+Ir_ke = vmistats['Ir_ke']
+
+idbinsa = [len(delays)-np.searchsorted(delays[::-1], n)[::-1] for n in dbinsa]
+
+Ir_Ke_dbinsa = np.zeros((len(dbinsa),Ir_ke.shape[1]))
+labels = []
+for i in range(0, len(dbinsa)):
+    Ir_Ke_dbinsa[i,:] = np.sum(Ir_ke[idbinsa[i][0]:idbinsa[i][1],:],0)/(idbinsa[i][1]-idbinsa[i][0]+1)  
+    labels.append("[{}...{}]ps".format(dbinsa[i][0], dbinsa[i][1]))
+
+fig, ax = plt.subplots()
+ax.set(xlabel = "ke, [eV]", title = "Evolution of trpes slices with p-p delay")
+    
+for i in range(0, len(dbinsa)):
+        ax.plot(KEs, Ir_Ke_dbinsa[i,:], label=labels[i])
+            
+leg = ax.legend()
+leg.set_draggable(1)
